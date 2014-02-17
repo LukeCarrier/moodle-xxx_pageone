bfe58c902038d09af202e173adcbb584aa01255a
Luke Carrier <luke@carrier.im>
2014-02-17 10:33:00 +0100

    I don't even want to know why

--- src/mdl2_generic.php
+++ src/mdl2_generic.php
@@ -1,14 +1,14 @@
 <?php
-function mdl21_getconfigparam($m, $v)
-{
- $config = get_config($m);
- if (eval('return isset($config->'.$v.');'))
-  return eval('return $config->'.$v.';');
- else
- {
-  global $CFG;
-  return eval('return $CFG->'.$m.'_'.$v.';');;
- }
+
+function mdl21_getconfigparam($m, $v) {
+    global $CFG;
+    $config = get_config($m);
+
+    if (isset($config->{$v})) {
+         return $config->{$v};
+    } elseif (isset($CFG->{"{$m}_{$v}"})) {
+        return $CFG->{"{$m}_{$v}"};
+    }
 }
 
 function mdl21_configparamisset($m, $v)
